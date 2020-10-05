071615d9e80442698b936e3ae5792295c30f7ddd
mahdieh-ahmadi <m119.ahmadi@gmail.com>
2020-10-05 21:19:08 +0330

    init folder

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useEffect  } from 'react';
+import {connect} from 'react-redux'
+import {BrowserRouter, Route , Switch} from 'react-router-dom'
+
+
+const App = () => {
+
+  return (
+    <BrowserRouter>
+    <div className="App">
+      {/* <Switch>
+      <Route path = '/Auth'  component={ Auth }/>
+      <Route path = '/Select_movie'  component={ Select }/>
+      <Route path = '/' component={ Main }/>
+      </Switch> */}
+      pleas start!
+      
+    </div>
+    </BrowserRouter>
+  )
+}
+
+export default App;
